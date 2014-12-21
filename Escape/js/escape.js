8ab6347c15dab2ae89133a4a3e0f6bc2ff0c0e17
Jeremy Vanseader <madoxlabs@gmail.com>
2014-12-21 00:59:00 -0500

    adding escape

---
+++ Escape/js/escape.js
@@ -0,0 +1,44 @@
+Game.appInit = function ()
+{
+}
+
+Game.deviceReady = function ()
+{
+}
+
+Game.loadingStart = function ()
+{
+}
+
+Game.loadingStop = function ()
+{
+}
+
+Game.appUpdate = function ()
+{
+
+}
+
+Game.appDrawAux = function ()
+{
+}
+
+Game.appDraw= function (eye)
+{
+}
+
+Game.appHandleKeyDown = function (event)
+{
+}
+
+Game.appHandleKeyUp = function (event)
+{
+}
+
+Game.appHandleMouseEvent = function(type, mouse)
+{
+}
+
+Game.appLoadingError = function (name)
+{
+}
